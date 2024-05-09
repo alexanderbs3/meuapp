84e4c18bfba2f407b156a355148b7a7c899489ac
José Mário Guimarães <alexander.cbss@hotmail.com>
2024-05-09 18:25:12 -0300

    teste

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Contador {
+    private int contador;
+
+    // Construtor
+    public Contador() {
+        contador = 0;
+    }
+
+    // Método para incrementar o contador
+    public void Incrementar() {
+        contador++;
+    }
+
+    // Método para decrementar o contador
+    public void Decrementar() {
+        contador--;
+    }
+
+    // Método para obter o valor atual do contador
+    public int ObterValor() {
+        return contador;
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        Contador meuContador = new Contador();
+
+        // Incrementando o contador algumas vezes
+        meuContador.Incrementar();
+        meuContador.Incrementar();
+        meuContador.Incrementar();
+
+        // Decrementando o contador algumas vezes
+        meuContador.Decrementar();
+
+        // Exibindo o valor atual do contador
+        Console.WriteLine("O valor atual do contador é: " + meuContador.ObterValor());
+    }
+}
